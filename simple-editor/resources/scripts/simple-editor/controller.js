61fb777150ecd5b0b3f77afe2be0c6afa33f332b
Louis Orleans <louis@orleans.io>
2016-03-04 03:02:35 -0800

    Adding more to the simple workflow editor

--- simple-editor/resources/scripts/simple-editor/controller.js
+++ simple-editor/resources/scripts/simple-editor/controller.js
@@ -72,6 +72,39 @@ var ngApp;
                         },
                         input: [],
                         output: []
+                    },
+                    {
+                        name: 'Containment damaged',
+                        icon: {icon: '/resources/images/potato.svg'},
+                        id: 12,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Humidity',
+                        icon: {icon: 'water-percent'},
+                        id: 17,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Raining',
+                        icon: {icon: 'weather-rain'},
+                        id: 18,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
                     }
                 ],
                 logics: [
@@ -84,7 +117,43 @@ var ngApp;
                             callback(input);
                         },
                         input: [],
-                        output: []
+                        output: [],
+                        options: [
+                            {
+                                name: 'Operators',
+                                type: 'spinner',
+                                choices: [
+                                    {
+                                        key: 'Equal',
+                                        value: function (input) {
+                                            return input == metric;
+                                        }
+                                    },
+                                    {
+                                        key: 'Approxomately',
+                                        value: function (input) {
+                                            return Math.abs(input - metric) < 1;
+                                        }
+                                    },
+                                    {
+                                        key: 'Greater Than',
+                                        value: function (input) {
+                                            return input > metric;
+                                        }
+                                    },
+                                    {
+                                        key: 'Less Than',
+                                        value: function (input) {
+                                            return input < metric;
+                                        }
+                                    }
+                                ]
+                            },
+                            {
+                                name: 'Metric',
+                                type: 'number'
+                            }
+                        ]
                     },
                     {
                         name: 'JSONify',
@@ -96,6 +165,37 @@ var ngApp;
                         },
                         input: [],
                         output: []
+                    },
+                    {
+                        name: 'State',
+                        icon: {icon: 'altimeter'},
+                        id: 15,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: [],
+                        options: [
+                            {
+                                name: 'States',
+                                type: 'spinner',
+                                choices: [
+                                    {
+                                        key: 'Low',
+                                        value: function (input) {
+                                            return input < 10;
+                                        }
+                                    },
+                                    {
+                                        key: 'High',
+                                        value: function (input) {
+                                            return input > 100;
+                                        }
+                                    }
+                                ]
+                            }
+                        ]
                     }
                 ],
                 endpoints: [
@@ -136,6 +236,50 @@ var ngApp;
                         name: 'SMS',
                         icon: {icon: 'message-text'},
                         id: 9,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Water Pump',
+                        icon: {icon: 'water-pump'},
+                        id: 13,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Phillips Hue',
+                        icon: {icon: 'lightbulb'},
+                        id: 14,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Twitter',
+                        icon: {icon: 'twitter'},
+                        id: 16,
+                        payload: '',
+                        logic: function (input, callback) {
+                            callback(input);
+                        },
+                        input: [],
+                        output: []
+                    },
+                    {
+                        name: 'Instagram',
+                        icon: {icon: 'instagram'},
+                        id: 16,
                         payload: '',
                         logic: function (input, callback) {
                             callback(input);
@@ -194,11 +338,42 @@ var ngApp;
                     data: {
                         accessKeyID: {
                             key: 'Access Key ID',
-                            value: 'AKIAIOSFODNN7EXAMPLE'
+                            value: 'AKIAIOSFODNN7EXAMPLE',
+                            hint: 'https://console.aws.amazon.com/iam/home?#security_credential'
                         },
                         secretAccessKey: {
                             key: 'Secret Access Key',
-                            value: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+                            value: 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY',
+                            hint: 'https://console.aws.amazon.com/iam/home?#security_credential'
+                        }
+                    }
+                },
+                {
+                    name: 'MongoDB Server',
+                    icon: {icon: 'database'},
+                    id: 6,
+                    type: 'credentials',
+                    data: {
+                        URL: {
+                            key: 'URL',
+                            value: 'ds035010.mlab.com:47030',
+                            hint: 'https://mlab.com/databases'
+                        },
+                        username: {
+                            key: 'Username',
+                            value: 'edyza_user',
+                            hint: 'https://mlab.com/databases'
+                        },
+                        password: {
+                            key: 'Password',
+                            value: 'password123',
+                            type: 'password',
+                            hint: 'https://mlab.com/databases'
+                        },
+                        database: {
+                            key: 'Database',
+                            value: 'edyza',
+                            hint: 'https://mlab.com/databases'
                         }
                     }
                 }
@@ -339,6 +514,68 @@ var ngApp;
 
                 var e = $compile(html)(scope);
                 element.replaceWith(e);
+            }
+        };
+    });
+
+    ngApp.directive('iconSelector', function ($compile) {
+        return {
+            restrict: 'E',
+            scope: {icon: '=icon'},
+            template: function () {
+                var html = '<div layout="row">' +
+                        '<workflow-icon rep="icon"></workflow-icon>' +
+                        '<md-input-container>' +
+                            '<label>Icon</label>' +
+                            '<input ng-model="icon.icon" type="text" />' +
+                        '</md-input-container>' +
+                    '</div>';
+
+                return html;
+            }
+        };
+    });
+
+    ngApp.directive('simpleNodeEditor', function ($mdDialog, $mdMedia) {
+        return {
+            restrict: 'E',
+            scope: {node: '=node'},
+            templateUrl: '/resources/parts/simple-editor/simple-node-editor.html',
+            link: function (scope, element, attrs) {
+                scope.openNodeOptions = function (ev) {
+                    var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
+
+                    function DialogController ($scope, $mdDialog) {
+                        $scope.node = scope.node;
+
+                        $scope.hide = function() {
+                            $mdDialog.hide();
+                        };
+                        $scope.cancel = function() {
+                            $mdDialog.cancel();
+                        };
+                        $scope.save = function() {
+                            $mdDialog.hide($scope.node);
+                        };
+                    }
+
+                    $mdDialog.show({
+                        controller: DialogController,
+                        templateUrl: 'resources/parts/simple-editor/dialog-node-editor.html',
+                        parent: angular.element(document.body),
+                        targetEvent: ev,
+                        clickOutsideToClose: false,
+                        fullscreen: useFullScreen
+                    }).then(function(node) {
+                        scope.node = node;
+                    });
+                    scope.$watch(function() {
+                            return $mdMedia('xs') || $mdMedia('sm');
+                        }, function(wantsFullScreen) {
+                            scope.customFullscreen = (wantsFullScreen === true);
+                        }
+                    );
+                };
             }
         };
     });
